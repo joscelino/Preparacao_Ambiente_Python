e86e6f764b13dd4c3cb700000283eb9b775da473
Joscelino Oliveira <joscelino.oliveira@gmail.com>
2021-03-01 21:26:02 -0300

    refactor: docstring of start.py file

--- src/start.py
+++ src/start.py
@@ -7,7 +7,7 @@ def soma(
     """Sum two values
     :param - valor_1: first value
            - valor_2: second value
-    :return - Sum of valor_1 and valor_2
+    :return - Sum of valor_1 and valor_2 only
     """
     soma_dois_valores = valor_1 + valor_2
     return soma_dois_valores
